131d2f37426fb2e4b7933fac44ae1f21dacaff62
npragin <npragin@gmail.com>
2026-02-23 00:08:25 -0800

    Add stuff for Stage

--- multi_agent_search/setup.py
+++ multi_agent_search/setup.py
@@ -1,3 +1,5 @@
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = "multi_agent_search"
@@ -9,6 +11,10 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", glob("launch/*.launch.py")),
+        ("share/" + package_name + "/config", ["config/config.toml", "config/robot_config.yaml"]),
+        ("share/" + package_name + "/world", glob("world/*.world")),
+        ("share/" + package_name + "/world/include", glob("world/include/*.inc")),
     ],
     package_data={"": ["py.typed"]},
     install_requires=["setuptools"],
